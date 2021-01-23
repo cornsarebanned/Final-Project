00e3e86f5a950dcf16208cd31e357a59e422bcbc
cornsarebanned <yqseah001@gmail.com>
2021-01-23 10:22:26 +0800

    slight amendment to cake_template.css/html/js(jon)

--- js/cake_template.js
+++ js/cake_template.js
@@ -1,7 +1,7 @@
 function load() {
     var urlParams = new URLSearchParams(window.location.search);
-    var image = "<img src = " + '"' + urlParams.get('img') + '" width="450" height="450">';
-    var price = "$" + urlParams.get('price')
+    var image = "<img src = "+'"'+urlParams.get('img')+'">';
+    var price = "$" + urlParams.get('price');
     document.getElementById('productName').innerHTML = urlParams.get('name');
     document.getElementById('productDesc').innerHTML = urlParams.get('desc');
     document.getElementById('productPrice').innerHTML = price;
--- js/cakes_testing.js
+++ js/cakes_testing.js
@@ -1,87 +1,85 @@
-var cakeProducts = [
+const cakeProduct = [
     {
         "productId": 1,
         "productName": "Blueberries and Vanilla Cake",
         "description": "Sandwiched between 2 Strawberry red coloured large macaron shells are Fresh Strawberries Cream and our Signature Chocolate Ganache filling.",
-        "price": 55.00,
-        "imageUrl": "images/cakes_images/blueRaspberryCrop_1_500x500.png"
+        "price": "$55.00",
+        "image": "images/cakes_images/blue_raspberry_cake.jpg"
     },
     {
         "productId": 2,
         "productName": "Blueberries Crusted Cheesecake",
         "description": "Three thick and rich layers of chocolate cake are stacked and immersed with generous amounts of mouth-watering Vanilla Buttercream. Last but not least, a beautiful melted dark chocolate drip is drizzled to create this masterpiece!",
-        "price": 65.00,
-        "imageUrl": "images/cakes_images/blueberries_syrup_lg_crop_500x500.jpg"
+        "price": "$65.00",
+        "image": "images/cakes_images/blueberry_cheesecake_sm.jpg"
     },
     {
         "productId": 3,
         "productName": "Fresh Pink Marbled Cake with Macarons",
         "description": "Dark Valrhona Chocolate generously filled with Nutella jam, in house cooked milk crumbles, Nutella infused velvet cream cheese, crowned with artisanal French Chocolate Bar.",
-        "price": 70.00,
-        "imageUrl": "images/cakes_images/cake_burnout_candles_sm_resize1_500x500.jpg"
+        "price": "$70.00",
+        "image": "images/cakes_images/cake_burnout_candles_sm.jpg"
     },
     {
         "productId": 4,
         "productName": "Strawberries and Minty Viola Cake",
         "description": "Sandwiched between 2 Strawberry red coloured large macaron shells are Fresh Strawberries Cream and our Signature Chocolate Ganache filling.",
-        "price": 49.00,
-        "imageUrl": "images/cakes_images/mint_strawberry_lg_crop_500x500.jpg"
+        "price": "$49.00",
+        "image": "images/cakes_images/mint_strawberry_lg.jpg"
     },
     {
         "productId": 5,
         "productName": "Vanilla Cake with Chocolate Drizzle",
         "description": "Pastel pink floral cake with edible Dried Rose Petals with Classic Vanilla Cream.",
-        "price": 69.90,
-        "imageUrl": "images/cakes_images/heritage_chocolate_cake.jpg"
+        "price": "$69.90",
+        "image": "images/cakes_images/small_heritage_chocolate_cake.jpg"
     },
     {
         "productId": 6,
         "productName": "Red Velvet Macarons Cake",
         "description": "Rich valrhona chocolate cake layers, frosted with chocolate fudge cream cheese, finished with a salted caramel sauce drizzle wreath and cocoa nibs.",
-        "price": 69.00,
-        "imageUrl": "images/cakes_images/macaron_cake.jpg"
+        "price": "$69.95",
+        "image": "images/cakes_images/macaron_cake.jpg"
     },
     {
         "productId": 7,
         "productName": "Cranberries Summer Peach Cake",
         "description": "Maple infused cake layerd with in house cooked Blueberry & Blackberry jam, fluffy smooth Cream Cheese Swirls and Fresh Berries.",
-        "price": 50.00,
-        "imageUrl": "images/cakes_images/xmas_redberry_cake.jpg"
+        "price": "$50.00",
+        "image": "images/cakes_images/xmas_redberry_cake.jpg"
     },
     {
         "productId": 8,
         "productName": "Raspberries and Chocolate Layered Cake",
         "description": "Adapted from the ever-so-popular Strawberries Shortcake, expect moist and Velvety Vanilla layers for this cake. Expect freshly Whipped Strawberries Chantilly and farm-picked sliced Strawberries.",
-        "price": 55.00,
-        "imageUrl": "images/cakes_images/xmas_chocolate_cake.jpg"
+        "price": "$55.00",
+        "image": "images/cakes_images/xmas_chocolate_cake.jpg"
     },
     {
         "productId": 9,
         "productName": "White Xmas Cake with Cherries and Cookies",
         "description": "Made with a Dark Stout Beer, Molasses, Ginger, Nutmeg, Cinnamon and Cardamom then topped with a Sweet Cream Cheese frosting. It's sweet, spicy, comforting and indulgent all at the same time.",
-        "price": 55.00,
-        "imageUrl": "images/cakes_images/gingerbread_cake.jpg"
+        "price": "$55.00",
+        "image": "images/cakes_images/gingerbread_cake.jpg"
     },
     {
         "productId": 10,
         "productName": "Rainbow Sprinkled Star Shaped Funfetti Cake",
         "description": "Our very own rendition of the traditional Black Forest Cake. Moist Belgian Dark Chocolate Cake Sponge, layered with Chocolate Fudge and Madagascar Vanilla bean chantilly, textured with in-house cooked balsamic cherry jam with juicy plump cherry!",
-        "price": 69.00,
-        "imageUrl": "images/cakes_images/sparkles_cake.jpg"
+        "price": "$69.95",
+        "image": "images/cakes_images/sparkles_cake.jpg"
     }
 ];
 
 document.getElementById("cakes").innerHTML = `
 <div id="container_products" class="container">
 <div class="product-container">
-
-${cakeProducts.map(function (cake) {
-    return ` 
-    <a href ="cake_template.html?img=${cake.imageUrl}&name=${cake.productName}&desc=${cake.description}&price=${cake.price}">
+${cakeProduct.map(function(cake) {
+    return` 
     <div class="indi-product">
         <div class="product-hvr">
             <div class="product">
-                <img class="product-img" src="${cake.imageUrl}">
+                <img class="product-img" src= ${cake.image}>
             </div>
             <div class="details">
                 <div class="content">
@@ -91,12 +89,30 @@ ${cakeProducts.map(function (cake) {
             </div>
         </div>
         <p class="product-description">${cake.productName}</p>
-        <p class="product-price">From $${cake.price}</p>
+        <p class="product-price">From ${cake.price}</p>
     </div>
-    </a>
 `
 }).join('')}
 </div>
     </div>
 `
 
+// const cakeProduct = [];
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        // document.getElementById("demo").innerHTML = xhttp.responseText;
+//        var response = JSON.parse(xhttp.responseText);
+//        console.log(response.cakeProduct);
+//     }
+// };
+// xhttp.open("GET", "json/cakes_testing.json", true);
+// xhttp.send();
+
+// fetch("json/cakes_testing.json")
+//     .then(response => response.json())
+//     .then(data =>{
+//         console.log(data);
+//         document.querySelector("#cakes").innerText = data
+//     })
