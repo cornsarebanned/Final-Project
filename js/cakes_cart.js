7a91dbafcaa7e522b68d70da661f94fc3f3ca1e2
mrvincenttan <73478468+mrvincenttan@users.noreply.github.com>
2021-02-12 17:16:09 +0800

    updated cart page, description in full, removed cake quantity selection here, customer to choose how many cake to buy from cake_template page, display chosen cake quantity, display calculated sub total cost for each cake type.

--- js/cakes_cart.js
+++ js/cakes_cart.js
@@ -7,6 +7,9 @@ let myCakeProducts = [
     productName: "Blueberries and Vanilla Cake",
     description: "Sandwiched between 2 Strawberry red coloured large macaron shells are Fresh Strawberries Cream and our Signature Chocolate Ganache filling.",
     price: 55.00,
+    cakesQuantity: 1,
+    cakeSize: 6, // cannot have hyphen for JSON in cake-size
+    candles: 2,
     imageUrl: "images/cakes_images/blueRaspberryCrop_1_500x500.png"
   },
   {
@@ -14,6 +17,9 @@ let myCakeProducts = [
     productName: "Blueberries Crusted Cheesecake",
     description: "Three thick and rich layers of chocolate cake are stacked and immersed with generous amounts of mouth-watering Vanilla Buttercream. Last but not least, a beautiful melted dark chocolate drip is drizzled to create this masterpiece!",
     price: 65.00,
+    cakesQuantity: 2,
+    cakeSize: 8,
+    candles: 3,
     imageUrl: "images/cakes_images/blueberries_syrup_lg_crop_500x500.jpg"
   },
   {
@@ -21,6 +27,9 @@ let myCakeProducts = [
     productName: "Fresh Pink Marbled Cake with Macarons",
     description: "Dark Valrhona Chocolate generously filled with Nutella jam, in house cooked milk crumbles, Nutella infused velvet cream cheese, crowned with artisanal French Chocolate Bar.",
     price: 70.00,
+    cakesQuantity: 3,
+    cakeSize: 10,
+    candles: 4,
     imageUrl: "images/cakes_images/cake_burnout_candles_sm_resize1_500x500.jpg"
   },
   {
@@ -28,6 +37,9 @@ let myCakeProducts = [
     productName: "Strawberries and Minty Viola Cake",
     description: "Sandwiched between 2 Strawberry red coloured large macaron shells are Fresh Strawberries Cream and our Signature Chocolate Ganache filling.",
     price: 49.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/mint_strawberry_lg_crop_500x500.jpg"
   },
   {
@@ -35,6 +47,9 @@ let myCakeProducts = [
     productName: "Vanilla Cake with Chocolate Drizzle",
     description: "Pastel pink floral cake with edible Dried Rose Petals with Classic Vanilla Cream.",
     price: 65.99,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/heritage_chocolate_cake_crop_500x500.jpg"
   },
   {
@@ -42,6 +57,9 @@ let myCakeProducts = [
     productName: "Red Velvet Macarons Cake",
     description: "Sandwiched between 2 Strawberry red coloured large macaron shells are Fresh Strawberries Cream and our Signature Chocolate Ganache filling.",
     price: 55.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/macaron_cake_crop_500x500.jpg"
   },
   {
@@ -49,6 +67,9 @@ let myCakeProducts = [
     productName: "Cranberries Summer Peach Cake",
     description: "Maple infused cake layerd with in house cooked Blueberry & Blackberry jam, fluffy smooth Cream Cheese Swirls and Fresh Berries.",
     price: 50.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/xmas_redberry_crop_500x500.jpg"
   },
   {
@@ -56,6 +77,9 @@ let myCakeProducts = [
     productName: "Raspberries and Chocolate Layered Cake",
     description: "Adapted from the ever-so-popular Strawberries Shortcake, expect moist and Velvety Vanilla layers for this cake. Expect freshly Whipped Strawberries Chantilly and farm-picked sliced Strawberries.",
     price: 55.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/xmas_chocolate_cake_crop_500x500.jpg"
   },
   {
@@ -63,6 +87,9 @@ let myCakeProducts = [
     productName: "White Xmas Cake with Cherries and Cookies",
     description: "Made with a Dark Stout Beer, Molasses, Ginger, Nutmeg, Cinnamon and Cardamom then topped with a Sweet Cream Cheese frosting. It's sweet, spicy, comforting and indulgent all at the same time.",
     price: 55.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/gingerbread_cake_lg_crop_1_500x500.jpg"
   },
   {
@@ -70,12 +97,19 @@ let myCakeProducts = [
     productName: "Rainbow Sprinkled Star Shaped Funfetti Cake",
     description: "Our very own rendition of the traditional Black Forest Cake. Moist Belgian Dark Chocolate Cake Sponge, layered with Chocolate Fudge and Madagascar Vanilla bean chantilly, textured with in-house cooked balsamic cherry jam with juicy plump cherry!",
     price: 69.00,
+    cakesQuantity: 1,
+    cakeSize: 6,
+    candles: 1,
     imageUrl: "images/cakes_images/sparkles_cake_lg_crop1_500x500.jpg"
   }
 ];
 
 // store arrays in local storage as key: cakesData
 localStorage.setItem("cakesData",JSON.stringify(myCakeProducts));
+    const inch8 = 12; // add $12 for 8inch cake choice
+    const inch10 = 16; // add $16 for 12inch cake choice
+    var selectedCakeSizeCost = 0; // updated cost of cake, depending on chosen cake size
+    var subTotalCakeCost = 0;
 
     let output = "";
     // get stored data from in local storage
@@ -83,6 +117,18 @@ localStorage.setItem("cakesData",JSON.stringify(myCakeProducts));
     console.log(storedCakesData);
 
     for (let i = 0; i < storedCakesData.length; i++) {
+      subTotalCakeCost = (storedCakesData[i].price * storedCakesData[i].cakesQuantity);
+      switch(storedCakesData[i].cakeSize) {
+        case 8:
+          selectedCakeSizeCost = storedCakesData[i].price + inch8;
+          break;
+        case 10:
+          selectedCakeSizeCost = storedCakesData[i].price + inch10;
+          break;
+        default: // 6inch here
+          selectedCakeSizeCost = storedCakesData[i].price;
+      } 
+    
     output +=
     `
     <div class="container bg-white py-2 my-3">
@@ -90,24 +136,24 @@ localStorage.setItem("cakesData",JSON.stringify(myCakeProducts));
     <div class="row mb-3">
       <div class="col-12 col-sm-6">
         <div class="row">
-          <div class="col-sm-6 col-md-3 mb-2">
+          <div class="col-sm-6 col-md-3 mb-2 cartSmallScreenImage">
             <img src="${storedCakesData[i].imageUrl}"
                 alt="${storedCakesData[i].productName}"
                 class="rounded img-fluid d-block mx-auto" style="border:2px solid black;">
           </div>
           <div class="cart_product_font col-sm-6 col-md-8 mb-2">
-          ${storedCakesData[i].productName} </div>
+          ${storedCakesData[i].productName}, ${storedCakesData[i].cakeSize} inch, 
+          with ${storedCakesData[i].candles} candle(s)</div>
         </div>
       </div>
       <div class="col-12 col-sm-6">
         <div class="row">
           <div class="cart_productprice_font col-sm-3 text-left mb-2">
-            $${storedCakesData[i].price}  </div> 
+            $${selectedCakeSizeCost}  </div> 
           <div class="col-sm-3 text-left mb-2">
-            <input type="number" name="quantity" value="1" size="1" min="0" max="10" class="form-control quantity-input"
-              data-original="1">
+            ${storedCakesData[i].cakesQuantity} cakes =
           </div>
-          <div class="cart_totalproductprice_font col-sm-4 text-left mb-2">$${storedCakesData[i].price}</div>
+          <div class="cart_totalproductprice_font col-sm-4 text-left mb-2">$${subTotalCakeCost}</div>
           <div class="col-sm-2 text-right mb-2">
             <button type="button" class="btn btn-danger"><i
                 class="fa fa-trash"></i></button>
